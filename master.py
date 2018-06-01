eb9b76315a798a8075b81a37485f7e0b4616b208
mukulkadel <mukulkadel@gmail.com>
2018-06-02 01:26:59 +0530

    first draft

---
+++ master.py
@@ -0,0 +1,48 @@
+import socket,time,array
+
+slave_ip = "127.0.0.1"
+port = 6994
+
+def get_data(s):
+    data = []
+    l = int(s.recv(40))
+    while True:
+        if l <= 0:
+            break
+        elif l > 512:
+            tmp = s.recv(512)
+        else:
+            tmp = s.recv(l)
+        data.append(tmp)
+        l -= len(tmp)
+
+    tmp = b"".join(data)
+    data = tmp.decode()
+    return float(data)
+
+def get_result(arr):
+    arr = array.array('l',arr)
+    s = socket.socket()
+    s.connect((slave_ip,port))
+    tmp = arr.tobytes()
+    l_tmp = str(len(tmp))
+    s.send(("0"*(40-len(l_tmp))+l_tmp).encode())
+    s.send(tmp)
+    result = get_data(s)
+    s.close()
+    return result
+
+if __name__=="__main__":
+    arr = list(range(100000))
+
+    #for verification
+    old_t = time.time()
+    print("Master result: {}".format(sum(arr)))
+    new_t = time.time()
+    print("Normal way, took: {}".format(new_t-old_t))
+
+    #calling slaves to process data
+    old_t = time.time()
+    print("Network result: {}".format(get_result(arr)))
+    new_t = time.time()
+    print("Over network, took: {}".format(new_t - old_t))
---
+++ slave.py
@@ -0,0 +1,48 @@
+import socket,array
+import multiprocessing as mp
+
+server_ip = "127.0.0.1"
+port = 6994
+
+def get_data(s):
+    data = []
+    l = int(s.recv(40))
+    while True:
+        if l <= 0:
+            break
+        elif l > 512:
+            tmp = s.recv(512)
+        else:
+            tmp = s.recv(l)
+        data.append(tmp)
+        l -= len(tmp)
+
+    tmp = b"".join(data)
+    data = array.array('l',[])
+    data.frombytes(tmp)
+    return data.tolist()
+
+def func(data):
+    s = sum(data)
+    return s
+
+if __name__ == "__main__":
+    s = socket.socket()
+    s.bind((server_ip,port))
+    s.listen(5)
+    print("Ready to serve master.")
+
+    while True:
+        c,addr = s.accept()
+        print("Connected with master {}".format(addr))
+        data = get_data(c)
+        print("Data received")
+        result = str(func(data))
+        tmp = result.encode()
+        l_tmp = str(len(tmp))
+        c.send(("0"*(40-len(l_tmp))+l_tmp).encode())
+        c.send(tmp)
+        c.close()
+        print("done.")
+        break
+    s.close()
